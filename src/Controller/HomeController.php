221579a22cb523e4ddf435fcea280e10c4afffbe
Isandre47 <nuanda@live.fr>
2022-07-13 17:10:07 +0200

    suite ajout des divers élements de la page comment ca marche et modif avec les assets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function home(): Response
     {
-        return $this->render('base.html.twig', [
+        return $this->render('static/home.html.twig', [
+            'page_title' => 'home',
+            'user' => 'toto',
+            'nav_type' => 'std',
+        ]);
+    }
+    #[Route('/comment-ca-marche', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('static/about.html.twig', [
             'page_title' => 'home',
             'user' => 'toto',
             'nav_type' => 'std',
